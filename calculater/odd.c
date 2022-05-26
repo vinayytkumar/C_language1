3c3305acd064afe20e11caa91459ed2dcca21845
shubhamkumar <vinayytrana@gmail.com>
2022-05-26 21:57:53 +0530

    first co

---
+++ calculater/odd.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    printf("Enter the number = ");
+    scanf("%d",&a);
+    if(a%2==2){
+        printf("The number is even");
+    }
+    else{
+        printf("The number is odd");
+    }
+
+    return 0;
+}
