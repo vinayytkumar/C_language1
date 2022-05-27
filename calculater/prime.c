0fd1dcc33942cd17f165e524c0fcec073f7aa1e3
shubhamkumar <vinayytrana@gmail.com>
2022-05-27 11:11:45 +0530

    fir]sc

--- calculater/prime.c
+++ calculater/prime.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 
 int main(){
-    int n;
-    printf("Enter the number = ");
-    scanf("%d", &n);
+   int n;
+   int isPrime = 1;
 
-    if (n%2!=0)
-    {
-    printf("The number is prime");
-    }
-    else{
-        printf("The number is not prime");
-    }
+   printf("Enter the number = ");
+   scanf("%d",&n);
+   for (int i = 2; i*i < n; i++)
+   {
+       if(n%i==0){
+           isPrime = 0;
+       }
+   }
+   if(isPrime){
+       printf("The number %d is prime\n",n);
+   }
+   else{
+       printf("The number %d is not prime\n",n);
+   }
 
     return 0;
 }
