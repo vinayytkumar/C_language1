a1972a1e73b1aa26e82795a4f7304779766d18b2
shubhamkumar <vinayytrana@gmail.com>
2022-07-11 13:07:52 +0530

    pp

--- chapter7array/array_reverse.c
+++ chapter7array/array_reverse.c
@@ -1,34 +1,35 @@
 #include <stdio.h>
-// function decleration 
-void reverse(int arr[],int n);
-void print(int arr[],int n);
 
+// function decleration
+void reverse(int arr[], int n);
+void print(int arr[], int n);
 
-int main(){
-int arr[] = { 1, 2, 3, 4, 5};
-reverse(arr,5);
-print(arr,5);
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5};
+    reverse(arr, 5);
+    print(arr, 5);
     return 0;
 }
 
-
 // function data
 
-void print(int arr[],int n){
+void print(int arr[], int n)
+{
     for (int i = 0; i < n; i++)
     {
-        printf("%d\t",arr[i]);
+        printf("%d\t", arr[i]);
     }
     printf("\n");
 }
 
-void reverse(int arr[],int n){
-    for (int i = 0; i < n/2; i++)
+void reverse(int arr[], int n)
+{
+    for (int i = 0; i < n / 2; i++)
     {
-      int firstvalue = arr[i];
-      int secondvalue = arr[n-i-1];
-      arr[i] = secondvalue;
-      arr[n-i-1] = firstvalue;
+        int firstvalue = arr[i];
+        int secondvalue = arr[n - i - 1];
+        arr[i] = secondvalue;
+        arr[n - i - 1] = firstvalue;
     }
-    
 }
